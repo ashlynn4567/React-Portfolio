4de35cec6cceb25ac83ac5e389f86e2863759589
Ashley's PC <ashlynn4567@gmail.com>
2022-07-10 23:46:23 -0500

    updated portfolio with some basic components to test connection and rendering

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+    return(
+        <div>
+            <h1>Who am I?</h1>
+            <p>I am here!</p>
+        </div>
+    );
+};
+
+export default About;
